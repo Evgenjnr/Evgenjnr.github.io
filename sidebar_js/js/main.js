624aed5a5b7b897d41a0f974602f8517eaf9f0d5
evgenjnr <evgeniyr7@gmail.com>
2018-03-14 22:38:34 +0200

    after pull

--- sidebar_js/js/main.js
+++ sidebar_js/js/main.js
@@ -1,16 +1,24 @@
 window.addEventListener('DOMContentLoaded', init);
 function init() {
-	showElem('.pop_menu', '.active_menu');
+	showElem('.pop_menu', '.active_menu', 'pop_menu--show');
 }
+
+ let computedStyle = getComputedStyle(document.querySelector('.page_footer'));
+ let computed = getComputedStyle(document.querySelector('.pop_menu'));
+ let footHeight = computedStyle.height;
+ let menuHeight = computed.height;
+ console.log(menuHeight);
+ let setElHeight = document.querySelector('.pop_menu');
+ setElHeight.style.height = 'menuHeight - footHeight';
 
 // /////////////
 
-function showElem(par1, par2){
+function showElem(par1, par2, par3){
 	let menu = document.querySelector(par1);
 	let actElem = document.querySelector(par2);
 	// console.log(menu);
 	// console.log(actEl);
 	actElem.addEventListener('click', function(){
-		menu.classList.toggle('pop_menu--show');
+		menu.classList.toggle(par3);
 	});
 }
