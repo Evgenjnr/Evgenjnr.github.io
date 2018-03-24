36b6e4a03419b70e33bba7460f36d4a70c93ab28
evgenjnr <evgeniyr7@gmail.com>
2018-03-24 16:35:42 +0200

    trening

---
+++ scroll_paralx/js/main.js
@@ -0,0 +1,40 @@
+$(window).on('resize load', function() {
+	let viewportHeight 	= $('.viewport').innerHeight();
+	let contentHeight 	= $('.container').innerHeight();
+	let multiplier      = (viewportHeight *100)/contentHeight;
+	
+
+
+	console.log('viewportHeight' +' ' + viewportHeight);
+	console.log('contentHeight' + ' ' + contentHeight);
+	console.log('multiplier' + ' ' + multiplier);
+
+
+    
+	if (contentHeight <= viewportHeight) {
+		$('.scrlbr').hide();
+	}
+	else {
+		$('.scrlbr').height(((viewportHeight * multiplier)/100)-2);
+	}
+	let scrHght = $('.scrlbr').height();
+	console.log(scrHght);
+
+
+		
+	$('.viewport').scroll(function(){
+		    let i 	= $(this).scrollTop();
+			console.log(i);
+			$('.scrlbr').css({transform:`translateY(${(i * multiplier)/100}px)`});
+	});
+
+	$('body').mousemove(function(ev){
+		let x = ev.pageX;
+		let y = ev.pageY;
+		console.log(x);
+		console.log(y);
+		$('.bgimg').css({top :`${(-x /100)}px`, left:`${(-y /100)}px`});
+	});
+		
+	
+});
