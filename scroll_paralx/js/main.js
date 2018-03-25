d2478b3d1ada568ae4480ba763d3e832d94ef82a
evgenjnr <evgeniyr7@gmail.com>
2018-03-25 10:17:58 +0300

    что то change code js and css

--- scroll_paralx/js/main.js
+++ scroll_paralx/js/main.js
@@ -1,8 +1,22 @@
 $(function(){
-	let viewport 	    = $('.viewport');
-	let container 	    = $('.container');
 
-$(window).on('resize load', function() {
+	let viewport, container, scrlbr, body; 
+
+	$(document).ready(function(){
+     	viewport 	    = $('.viewport');
+	 	container 	    = $('.container');
+	 	scrlbr 			= $('.scrlbr');
+
+		calc();
+
+		$(window).on('resize', function() {
+
+				  calc();
+		});
+
+	});
+
+function calc(){
 	let viewportHeight 	= viewport.innerHeight();
 	let contentHeight 	= container.innerHeight();
 	let multiplier      = (viewportHeight *100)/contentHeight;
@@ -16,30 +30,32 @@ $(window).on('resize load', function() {
 
     
 	if (contentHeight <= viewportHeight) {
-		$('.scrlbr').hide();
+		$(scrlbr).hide();
 	}
 	else {
-		$('.scrlbr').height(((viewportHeight * multiplier)/100)-2);
+		$(scrlbr).height(((viewportHeight * multiplier)/100)-2);
 	}
-	let scrHght = $('.scrlbr').height();
+	let scrHght = $(scrlbr).height();
+
 	console.log('высота бегунка' + ' ' + scrHght);
 
 
 		
-	$('.viewport').scroll(function(){
+		$(viewport).scroll(function(){
 		    let i 	= $(this).scrollTop();
 			console.log('scrollTop' + ' ' + i);
-			$('.scrlbr').css({transform:`translateY(${(i * multiplier)/100}px)`});
-	});
-});
-	$('body').mousemove(function(ev){
+			$(scrlbr).css({transform:`translateY(${(i * multiplier)/100}px)`});
+		});
+}
+	body = $('body');
+	$(body).mousemove(function(ev){
 		let x = ev.pageX;
 		let y = ev.pageY;
 		console.log(x);
 		console.log(y);
 		$('.bgimg').css({left :`${-(x /100)}px`});
 	});
-	$('body').mouseleave(function(){
+	$(body).mouseleave(function(){
 		
 		$('.bgimg').css({left :`0px`});
 	});
