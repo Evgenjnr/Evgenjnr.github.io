16732599934754a38300b13353475eefacf068eb
evgenjnr <evgeniyr7@gmail.com>
2018-05-15 18:11:47 +0300

    js rebild

--- weatherapp_land/js/main.js
+++ weatherapp_land/js/main.js
@@ -3,9 +3,10 @@ window.addEventListener('DOMContentLoaded', init);
 let menuBtn     = document.querySelector('.bar_menu'),
 	sectionElmn = document.querySelectorAll('section'),
 	mainHeader  = document.querySelector('.main_header'),
-	btnToDown   = document.querySelector('.nav-to-bottom span'),
-	btnDownLd   = document.querySelector('.btn-dwld'),
-	btnFeature   = document.querySelector('.btn-ftrs');
+	btnToDown   = document.querySelector('.btn_dwn'),
+	btnDownLd   = document.querySelector('.bnt_dwnld'),
+	btnFeature  = document.querySelector('.bnt_features'),
+	navItem     = document.querySelectorAll('.nav_item a');
 function init () {
 	let setPadding = function paddTop() {
 		let paddTop = getComputedStyle(mainHeader).height;
@@ -28,22 +29,38 @@ function init () {
 		}
 		
 	});
+	for(let target of navItem){
+		target.addEventListener('click', function(){
+						event.preventDefault();
+			let targetAttr = this.getAttribute('data-name');
+			for (let showElm of sectionElmn) {
+				if(showElm.getAttribute('data-name') == targetAttr){
+				let paddTop = getComputedStyle(mainHeader).height;
+				showElm.style.paddingTop = paddTop;
+				showElm.scrollIntoView({behavior: 'smooth'});
+				}
+			}
+		});
+	}
+
 
 	btnDownLd.addEventListener('click', function () {
-		let actionElm = document.querySelector('.download_wrap');
+		let actionElm = document.querySelector('.download');
 		let paddTop = getComputedStyle(mainHeader).height;
 		actionElm.style.paddingTop = paddTop;
-		actionElm.scrollIntoView();
+		actionElm.scrollIntoView({behavior: 'smooth'});
 	});
 	btnFeature.addEventListener('click', function () {
 		let actionElm = document.querySelector('.features');
 		let paddTop = getComputedStyle(mainHeader).height;
 		actionElm.style.paddingTop = paddTop;
-		actionElm.scrollIntoView();
+		actionElm.scrollIntoView({behavior: 'smooth'});
 	});
 	btnToDown.addEventListener('click', function () {
 		let actionElm = document.querySelector('.widjet');
-		actionElm.scrollIntoView();
+		let paddTop = getComputedStyle(mainHeader).height;
+		actionElm.style.paddingTop = paddTop;
+		actionElm.scrollIntoView({behavior: 'smooth'});
 	});
 	
 	
