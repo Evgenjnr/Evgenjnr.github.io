ef813843de40a2093083615a52df411775f46a80
evgenjnr <evgeniyr7@gmail.com>
2018-05-20 14:57:52 +0300

    modifed html & js

--- weatherapp_land/js/main.js
+++ weatherapp_land/js/main.js
@@ -1,27 +1,43 @@
 window.addEventListener('DOMContentLoaded', init);
 
 let menuBtn     = document.querySelector('.bar_menu'),
-	sectionElmn = document.querySelectorAll('section'),
 	mainHeader  = document.querySelector('.main_header'),
-	btnToDown   = document.querySelector('.btn_dwn'),
-	btnDownLd   = document.querySelector('.bnt_dwnld'),
-	btnFeature  = document.querySelector('.bnt_features'),
-	navItem     = document.querySelectorAll('.nav_item a');
+	sectionElmn = document.querySelectorAll('section'),
+	btnShow     = document.querySelectorAll('button'),
+	navItem     = document.querySelectorAll('.nav_item');
 function init () {
-
-	//set topPadding for each section element eq height header element
+//global set topPadding for each section element eq height of header element
 	let setPadding = function paddTop() {
 		let paddTop = getComputedStyle(mainHeader).height;
-		for (let i = 0; i< sectionElmn.length; i++) {
-		sectionElmn[i].style.paddingTop = paddTop;
+		for (let item of sectionElmn) {
+		item.style.paddingTop = paddTop;
 	}
+	// */end set topPadding for each section element eq height of header element
 	}
-
+	let setColor = function(){
+		for (let showElm of sectionElmn) {
+				if(showElm.getBoundingClientRect().top < 600 
+					&& showElm.getBoundingClientRect().top >= 0){
+					let sectionInView = showElm.getAttribute('data-name');
+				  for(let target of navItem){				  	
+				  	if(target.getAttribute('data-name') == sectionInView){
+				  		
+				  		target.firstChild.classList.add('active--item');				  	
+				  		}else{
+				  			target.firstChild.classList.remove('active--item');
+				  		}
+				  	}
+				  
+				}
+			}
+		}
 
 	window.addEventListener('load', setPadding);
 	window.addEventListener('resize', setPadding);
+	window.addEventListener('scroll', setColor);
 	
-	// */end set topPadding for each section element eq height header element
+
+
 	// hide/show navMenu in header for mobile	
 	menuBtn.addEventListener('click', function () {
 		let menuList = document.querySelector('.nav_list');
@@ -34,45 +50,53 @@ function init () {
 	});
 	// */end hide/show navMenu in header for mobile	
 
-	//add event 'click' for each element of navItem in header and footer 
-	// and scroll section element with data-name eq data-name navItem inTo view
+
+	viewSection(navItem);
+	viewSectionOnBttn(btnShow);
 	
-		for(let target of navItem){
+}
+// */end init
+
+//add event 'click' for each element of navItem in header and footer,
+// also button in top_section 
+// and scroll section element with data-name eq data-name navItem inTo view
+function viewSectionOnBttn(par1){
+		for(let target of par1){
 			target.addEventListener('click', function(ev){
 							ev.preventDefault();
 				let targetAttr = this.getAttribute('data-name');
 				for (let showElm of sectionElmn) {
 					if(showElm.getAttribute('data-name') == targetAttr){
-					// setPaddingTop (mainHeader, showElm);
 					showElm.scrollIntoView({behavior: 'smooth'});
 					}
 				}
+				
 			});
 		}
-		// end*/ add event 'click' for each element of navItem in header and footer 
-	// and scroll section element with data-name eq data-name navItem inTo view
-	
-	scrllElmnt(btnDownLd, '.download');
-	scrllElmnt(btnFeature, '.features');
-	scrllElmnt(btnToDown, '.widjet');	
-	
-
-	// function setPaddingTop (mainElm, targElm) {
-	// 	let paddTop = getComputedStyle(mainElm).height;
-	// 	targElm.style.paddingTop = paddTop;
-	// }
-
-	// function scrolling element after click on button in top_section
-
-	function scrllElmnt (elm1, elm2) {
-		elm1.addEventListener('click', function () {
-		let actionElm = document.querySelector(elm2);
-		// setPaddingTop (mainHeader, actionElm);
-		actionElm.scrollIntoView({behavior: 'smooth'});
-		});
 	}
 
-	// end*/ function scrolling element after click on button in top_section
-
-}
-// {behavior: 'smooth'}
+	function viewSection(par1){
+		for(let target of par1){
+			target.addEventListener('click', function(ev){
+							ev.preventDefault();
+				let siblings = this.parentNode.children;
+				for(let item of siblings){
+					if(item.firstChild.classList.contains('active--item')){
+						item.firstChild.classList.remove('active--item');
+					}
+						this.firstChild.classList.add('active--item');
+				}
+				
+				let targetAttr = this.getAttribute('data-name');
+				for (let showElm of sectionElmn) {
+					if(showElm.getAttribute('data-name') == targetAttr){
+					showElm.scrollIntoView({behavior: 'smooth'});
+					}
+				}
+				
+			});
+		}
+	}
+	
+// end*/ add event 'click' for each element of navItem in header and footer 
+// and scroll section element with data-name eq data-name navItem inTo view
