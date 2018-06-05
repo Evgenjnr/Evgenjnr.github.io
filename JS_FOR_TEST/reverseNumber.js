f61bc926bdcb2faaccc83beb800367d70376ec3b
evgenjnr <evgeniyr7@gmail.com>
2018-06-05 19:33:55 +0300

    add html, styles, rebuild js

--- JS_FOR_TEST/reverseNumber.js
+++ JS_FOR_TEST/reverseNumber.js
@@ -1,6 +1,27 @@
-window.addEventListener('DOMContentLoaded', reverseNumber);
-let someNumber = 5103851734;
-function reverseNumber () {
+window.addEventListener('DOMContentLoaded', init);
+ let outPut       = document.querySelector('#output');
+ let inputNumber = document.querySelector('#input');
+function init () {
+	let button = document.querySelector('.initButton')
+	button.addEventListener('click', function (ev) {
+		if(button.value == "Sort & Reverse"){
+			let getNumber = document.querySelector('#input').value;		
+			sortNreverse (getNumber);
+			button.value = "Clear";
+		}else{
+			button.value = "Sort & Reverse";
+			inputNumber.value = "";
+			outPut.value = "";
+		}
+		
+		
+	});
+	
+
+}
+
+
+function sortNreverse (inputNumber) {
 	/*let x         = String(someNumber);//преобразование числа в строку
 	let arr       = x.split('');//преобразование строки в массив
 	let newArr    = [];//пустой массив
@@ -10,14 +31,21 @@ function reverseNumber () {
 	let newNumber = Number(newString);//преобразование строки в число*/
 
 
-    let newArr = String(someNumber).split('').sort().reverse().join('');
+    let newArr = inputNumber.split('').sort().reverse().join('');
     let newNumber = Number(newArr);
+    console.log(typeof(newNumber));
 
-	let firstPar       = document.createElement('p');
-	firstPar.innerHTML = someNumber;
-	document.body.appendChild(firstPar);
+   
+    if(inputNumber.value != "" && !isNaN(newNumber)){
+    	outPut.value = newNumber;
+    }else{
+    	outPut.value = "Введите десятичное число";
+    }
+	
+	
+	
 
-	let secondPar       = document.createElement('p');
-	secondPar.innerHTML = newNumber;
-	document.body.appendChild(secondPar);
+	// let secondPar       = document.createElement('p');
+	// secondPar.innerHTML = newNumber;
+	// document.body.appendChild(secondPar);
 }
